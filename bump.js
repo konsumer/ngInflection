d8aa0b8dc8fb41b98f34a6715cae429191522b3b
David Konsumer <konsumer@jetboystudio.com>
2015-01-28 01:14:40 -0800

    bump to v1.1.3

--- bump.js
+++ bump.js
@@ -1,22 +1,33 @@
 var fs = require('fs');
 var exec = require('child_process').exec;
 
-exports = function(file, cb){
+function bump(file, cb){
   var pkg = JSON.parse(fs.readFileSync(file));
   var v = pkg.version.split('.');
   v[2]++;
   pkg.version = v.join('.');
-  fs.writeFile(file, JSON.stringify(pkg,null,2), function(err){
+  fs.writeFile(file, JSON.stringify(pkg,null,2), cb);
+}
+
+exports = function(cb){
+  bump(__dirname + '/package.json', function(err){
     if (err) return cb(err);
-    exec('git add -A && git commit -am "bump to v"' + pkg.version + ' && git tag v' + pkg.version + '&& git push --tags && git push', function(err, stdout, stderr){
+    bump(__dirname + '/bower.json', function(err){
       if (err) return cb(err);
-      cb(null, pkg.version);
+      fs.readFile(__dirname + '/package.json', function(err,p){
+        if (err) return cb(err);
+        var pkg = JSON.parse(p);
+        exec('git add -A && git commit -am "bump to v"' + pkg.version + ' && git tag v' + pkg.version + '&& git push --tags && git push', function(err, stdout, stderr){
+          if (err) return cb(err);
+          cb(null, pkg.version);
+        });
+      });
     });
   });
 };
 
 if (!module.parent) {
-  var ver = exports(__dirname + '/package.json', function(err, version){
+  var ver = exports(function(err, version){
     if (err){
       console.error(err);
       process.exit(1);
